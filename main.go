785f5daded7d848230ceba66ab4a5c907a43be26
imanimen <imanimen81@gmail.com>
2024-09-27 21:35:49 +0330

    update(gracefull shutdown)

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"context"
 	"go-microservice/handlers"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"time"
 )
 
@@ -26,5 +28,27 @@ func main() {
 		ReadTimeout: 1 * time.Second,
 		WriteTimeout: 1 * time.Second,
 	}
-	s.ListenAndServe()
+
+	go func ()  {
+		err := s.ListenAndServe()
+		if err != nil {
+			logger.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+	logger.Println("Received terminate, gracefully shutdown", sig)
+
+	// Create a context for shutdown with a timeout of 30 seconds
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+
+	// Shut down the server gracefully
+	err := s.Shutdown(tc)
+	if err != nil {
+		logger.Printf("Error during shutdown: %v", err)
+	}
 }
