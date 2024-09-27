c8373dfc306ae292e56e8f36a5b81897bbd545ba
imanimen <imanimen81@gmail.com>
2024-09-27 20:43:46 +0330

    chore(ServeMux): add servemux

--- main.go
+++ main.go
@@ -1,26 +1,19 @@
 package main
 
 import (
-	"fmt"
-	"io"
+	"go-microservice/handlers"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	// handler for our web server
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		log.Println("Hello World")
-		data, err := io.ReadAll(request.Body)
-		if err != nil {
-			http.Error(writer, "Oops", http.StatusBadRequest)
-			return
-		}
-		// write back to user
-		fmt.Fprintf(writer, "Hello %s\n", data) 
-	})
-	http.HandleFunc("/goodbye", func(writer http.ResponseWriter, request *http.Request) {
-		log.Println("Goodbye World")
-	})
-	http.ListenAndServe(":9090", nil)
+
+	logger := log.New(os.Stdout, "product-api", log.LstdFlags)
+	helloHandler := handlers.NewHello(logger)
+
+	serveMux := http.NewServeMux()
+	serveMux.Handle("/", helloHandler)
+
+	http.ListenAndServe(":9090", serveMux)
 }
