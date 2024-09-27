6317ec9faa8c1b294cbce94f73181bfc6ac1e85e
imanimen <imanimen81@gmail.com>
2024-09-27 19:35:08 +0330

    chore(section one): write back to user on the web server log

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 )
@@ -9,6 +11,13 @@ func main() {
 	// handler for our web server
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
 		log.Println("Hello World")
+		data, err := io.ReadAll(request.Body)
+		if err != nil {
+			http.Error(writer, "Oops", http.StatusBadRequest)
+			return
+		}
+		// write back to user
+		fmt.Fprintf(writer, "Hello %s\n", data)
 	})
 	http.HandleFunc("/goodbye", func(writer http.ResponseWriter, request *http.Request) {
 		log.Println("Goodbye World")
